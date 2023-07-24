72802f1af421860a14426112e88aec28210bf3b4
戴东阳 <2447328758@qq.com>
2023-07-24 16:09:20 +0800

    temp

---
+++ unpackage/dist/build/app-plus/static/temp/userCache.js
@@ -0,0 +1,30 @@
+var deviceKey = "devices"
+var genderDict={
+	// "-1":"",
+	"0":"女",
+	"1":"男"
+}
+async function getDevices(){
+	return await uni.getStorage({
+		key:deviceKey
+	})
+}
+
+function saveDevices(users){
+	uni.setStorage({
+		key:deviceKey,
+		data:users
+	})
+}
+
+function getUser(){
+	return {
+		did:"",
+		username:"",
+		gender:-1,
+		weight:0,
+		age:0
+	}
+}
+
+export{getDevices, saveDevices,genderDict,getUser}
